1d27c1c1f8bbe413ee4f200ec0edb74a23ea26ac
ibrahimmasoo <114830592+ibrahimmasoo@users.noreply.github.com>
2026-04-07 18:36:59 +0300

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8u5ndP8I_Xjy-prAbQEWDtmSMy0Ofv6o",
+  authDomain: "theramsis-1028b.firebaseapp.com",
+  projectId: "theramsis-1028b",
+  storageBucket: "theramsis-1028b.firebasestorage.app",
+  messagingSenderId: "586288818100",
+  appId: "1:586288818100:web:2963394db4ba63a4f7b4e1",
+  measurementId: "G-T2J11JFM82"
+};
